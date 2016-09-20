cda54147e1130fc83b6ab7432985f847dc57a929
johnpagetechnotects <emjohnpage@technotects.com/em>
2016-09-20 13:32:09 -0400

    Improved configuration and initialization feedback to users.

--- src/main/java/com/github/ptgoetz/logback/kafka/KafkaAppender.java
+++ src/main/java/com/github/ptgoetz/logback/kafka/KafkaAppender.java
@@ -14,88 +14,95 @@ import java.util.Properties;
 
 public class KafkaAppender extends AppenderBase<ILoggingEvent> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaAppender.class);
-    private Formatter formatter = new MessageFormatter();
-    private boolean logToSystemOut = false;
-    private String kafkaProducerProperties;
-    private String topic;
-    private KafkaProducer producer;
+  private static final Logger LOGGER = LoggerFactory.getLogger(KafkaAppender.class);
+  private Formatter formatter = new MessageFormatter();
+  private boolean logToSystemOut = false;
+  private String kafkaProducerProperties;
+  private String topic;
+  private KafkaProducer producer;
 
-    @Override
-    public void start() {
-        super.start();
-        LOGGER.info("Starting KafkaAppender...");
-        final Properties properties = new Properties();
-        try {
-            properties.load(new StringReader(kafkaProducerProperties));
-            producer = new KafkaProducer<>(properties);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-
-        if (topic == null) {
-            LOGGER.error("KafkaAppender requires a topic. Add this to the appender configuration.");
-            System.out.println("KafkaAppender requires a topic. Add this to the appender configuration.");
-        } else {
-            LOGGER.info("KafkaAppender will publish messages to the '{}' topic.",topic);
-            System.out.println("KafkaAppender will publish messages to the '" + topic + "' topic.");
-        }
-        LOGGER.info("kafkaProducerProperties = {}", kafkaProducerProperties);
-        LOGGER.info("Kafka Producer Properties = {}", properties);
-        if (logToSystemOut) {
-            System.out.println("KafkaAppender: kafkaProducerProperties = '" + kafkaProducerProperties + "'.");
-            System.out.println("KafkaAppender: properties = '" + properties + "'.");
-        }
+  @Override
+  public void start() {
+    super.start();
+    LOGGER.info("Starting KafkaAppender...");
+    final Properties properties = new Properties();
+    try {
+      properties.load(new StringReader(kafkaProducerProperties));
+      producer = new KafkaProducer<>(properties);
+    } catch (Exception exception) {
+      System.out.println("KafkaAppender: Exception initializing Producer. "+ exception +" : "+ exception.getMessage());
+      exception.printStackTrace();
+      throw new RuntimeException("KafkaAppender: Exception initializing Producer.",exception);
     }
-
-    @Override
-    public void stop() {
-        super.stop();
-        LOGGER.info("Stopping KafkaAppender...");
-        this.producer.close();
+    System.out.println("KafkaAppender: Producer intitialized: "+ producer);
+    if (topic == null) {
+      LOGGER.error("KafkaAppender requires a topic. Add this to the appender configuration.");
+      System.out.println("KafkaAppender requires a topic. Add this to the appender configuration.");
+    } else {
+      LOGGER.info("KafkaAppender will publish messages to the '{}' topic.",topic);
+      System.out.println("KafkaAppender will publish messages to the '" + topic + "' topic.");
     }
-
-    @Override
-    protected void append(ILoggingEvent event) {
-        String string = this.formatter.format(event);
-        if (logToSystemOut) {
-            System.out.println("KafkaAppender: Appending string: '" + string + "'.");
-        }
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, string);
-        this.producer.send(producerRecord);
+    LOGGER.info("kafkaProducerProperties = {}", kafkaProducerProperties);
+    LOGGER.info("Kafka Producer Properties = {}", properties);
+    if (logToSystemOut) {
+      System.out.println("KafkaAppender: kafkaProducerProperties = '" + kafkaProducerProperties + "'.");
+      System.out.println("KafkaAppender: properties = '" + properties + "'.");
     }
+  }
 
-    public Formatter getFormatter() {
-        return formatter;
-    }
+  @Override
+  public void stop() {
+    super.stop();
+    LOGGER.info("Stopping KafkaAppender...");
+    producer.close();
+  }
 
-    public void setFormatter(Formatter formatter) {
-        this.formatter = formatter;
+  @Override
+  protected void append(ILoggingEvent event) {
+    String string = this.formatter.format(event);
+    if (logToSystemOut) {
+      System.out.println("KafkaAppender: Appending string: '" + string + "'.");
     }
+    try {
+      ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, string);
+      producer.send(producerRecord);
+    }catch(Exception e){
+      System.out.println("KafkaAppender: Exception sending message: '" + e + " : "+ e.getMessage() + "'.");
+      e.printStackTrace();
+    }
+  }
 
-    public String getTopic() {
-        return topic;
-    }
+  public Formatter getFormatter() {
+    return formatter;
+  }
 
-    public void setTopic(String topic) {
-        this.topic = topic;
-    }
+  public void setFormatter(Formatter formatter) {
+    this.formatter = formatter;
+  }
 
-    public String getLogToSystemOut() {
-        return logToSystemOut + "";
-    }
+  public String getTopic() {
+    return topic;
+  }
 
-    public void setLogToSystemOut(String logToSystemOutString) {
-        if ("true".equalsIgnoreCase(logToSystemOutString)) {
-            this.logToSystemOut = true;
-        }
-    }
+  public void setTopic(String topic) {
+    this.topic = topic;
+  }
 
-    public String getKafkaProducerProperties() {
-        return kafkaProducerProperties;
-    }
+  public String getLogToSystemOut() {
+    return logToSystemOut + "";
+  }
 
-    public void setKafkaProducerProperties(String kafkaProducerProperties) {
-        this.kafkaProducerProperties = kafkaProducerProperties;
+  public void setLogToSystemOut(String logToSystemOutString) {
+    if ("true".equalsIgnoreCase(logToSystemOutString)) {
+      this.logToSystemOut = true;
     }
+  }
+
+  public String getKafkaProducerProperties() {
+    return kafkaProducerProperties;
+  }
+
+  public void setKafkaProducerProperties(String kafkaProducerProperties) {
+    this.kafkaProducerProperties = kafkaProducerProperties;
+  }
 }
